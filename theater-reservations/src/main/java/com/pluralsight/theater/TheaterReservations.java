b84f03afd98341092e18269dc0dc5e20ba5411f7
Ayanna Williams <ayanna.williams20@att.net>
2024-10-10 11:48:27 -0400

    updated exercise

--- theater-reservations/src/main/java/com/pluralsight/theater/TheaterReservations.java
+++ theater-reservations/src/main/java/com/pluralsight/theater/TheaterReservations.java
@@ -1,7 +1,6 @@
 package com.pluralsight.theater;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.Scanner;
 
 public class TheaterReservations {
